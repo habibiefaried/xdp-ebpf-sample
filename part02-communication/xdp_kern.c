3b413ef5ba657ce57ab9f836474b3de7532f5e8c
Habibie Faried <habibiefaried@gmail.com>
2020-01-06 11:55:26 +0700

    Fix

--- part02-communication/xdp_kern.c
+++ part02-communication/xdp_kern.c
@@ -10,26 +10,19 @@
 #include <linux/ipv6.h>
 
 typedef struct {
-	u32 I1;
+	u64 I1;
 	int I2;
 	char Sentence[256];
 } chown_event_t;
 
 BPF_PERF_OUTPUT(chown_events);
 
-
-char *strcpy_local(char *dest, const char *src) {
-	//taken from: https://github.com/torvalds/linux/blob/master/lib/string.c
-	char *tmp = dest;
-	while ((*dest++ = *src++) != '\0');
-	return tmp;
-} 
-
 int xdp_perf(struct xdp_md *ctx) {
 	chown_event_t event = {};
 	event.I1 = 500;
 	event.I2 = 1000;
-	strcpy_local(event.Sentence,"Hello World");
+	event.Sentence[0] = "H";
+	event.Sentence[1] = "\0";
 	chown_events.perf_submit(ctx, &event, sizeof(event));
 	return XDP_PASS;
 }
