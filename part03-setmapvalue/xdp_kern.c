448581bc67896051443c76201f4f40e12329154c
Habibie Faried <habibiefaried@gmail.com>
2020-01-07 10:30:54 +0700

    Fix

--- part03-setmapvalue/xdp_kern.c
+++ part03-setmapvalue/xdp_kern.c
@@ -7,7 +7,7 @@
 #include <uapi/linux/udp.h>
 #include <bcc/proto.h>
 
-BPF_HASH(cache, char[], char[], 256);
+BPF_HASH(cache, char *, char *, 256);
 
 int xdp_hash(struct xdp_md *ctx) {
 	return XDP_PASS;
